fa895e022b5ed216523f18d14ef3845434d6cb18
Evolve <infoevolvecompany@gmail.com>
2025-08-18 14:54:51 +0200

    feat: add 8-bit parallax styles and progress component

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,7 @@ import "./styles/forms.css";
 import "./styles/navbar.css";
 import "./styles/footer.css";
 import "./styles/pages.css";
+import "./styles/home.css";
 import "./styles/icons.css";
 import "./styles/game.css";
 import "./styles/magazzino.css";
---
+++ components/ScrollProgress.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+export default function ScrollProgress() {
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const totalScroll = document.body.scrollHeight - window.innerHeight;
+      const currentScroll = window.scrollY;
+      const scrolled = (currentScroll / totalScroll) * 100;
+      setProgress(scrolled);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    handleScroll();
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div className="progress-container">
+      <div className="progress-bar" style={{ height: `${progress}%` }} />
+    </div>
+  );
+}
